a2b1adb9722a42d19f323ad7fc96d36165dcfed2
JP3Pe <45834750+JP3Pe@users.noreply.github.com>
2022-09-06 22:31:32 +0900

    블로그 게시물을 리스트 형식으로 보여주는 기능 추가 (#12)
    # 변경 내역
    
    1. resolved #11

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,31 @@
-import HTMLReactParser from "html-react-parser";
-import Blog from "./blogs/[title]";
+import { readdirSync } from "fs";
+import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
+import Link from "next/link";
+import { join } from "path";
 
-function Home() {
-  return;
+function Home({ blogTitles }: Params) {
+  return (
+    <div className="blog__list">
+      <h1>Blog post list</h1>
+      <ul>
+        {blogTitles.map((blogTitle: string) => (
+          <li key={blogTitle}>
+            <Link href={`/blogs/${encodeURIComponent(blogTitle)}`}>
+              <a>{blogTitle}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+export const getStaticProps = async () => {
+  const blogsFromDirectory = readdirSync(join(process.cwd(), "blogs"));
+
+  return {
+    props: { blogTitles: blogsFromDirectory },
+  };
+};
 
 export default Home;
