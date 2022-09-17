f22d5c7f238af94582b80865d36d7bd08d275ddc
JP3Pe <rlaworhkd430430@gmail.com>
2022-09-17 16:25:39 +0900

    add: 기본적인 CSS 파일 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import type { AppProps } from "next/app";
 
+import "../styles/reset.css";
+import "../styles/root.css";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
