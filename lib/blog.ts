8bf31038298ad8e353300965d870e02ed9491308
김재광 <45834750+JP3Pe@users.noreply.github.com>
2022-09-24 19:46:12 +0900

    게시물 리스트를 grid 형식으로 표시하기 (#35)
    * add: 메인 화면에 게시물과 내용 일부 함께 보여주는 기능 추가
    
    closes #34
    
    * up Blog 관련 코드 1개의 클래스로 통합
    
    * add: grid 형식의 css 속성 추가
    
    글 제목과 글 내용이 메인 화면에 보이게 추가
    
    resolved #34

--- lib/blog.ts
+++ lib/blog.ts
@@ -4,22 +4,31 @@ import { join } from "path";
 import { remark } from "remark";
 import remarkHtml from "remark-html";
 
-export class Blogs {
+export class Blog {
   public static readonly CONTENT_DIRECTORY_NAME: string = "blogs";
   public static readonly PUBLIC_DIRECTORY_NAME: string = "public";
   public static readonly CONTENT_FORMAT: string = ".md";
   public static readonly CONTENT_DIRECTORY_PATH: string = join(
     process.cwd(),
     this.PUBLIC_DIRECTORY_NAME,
-    Blogs.CONTENT_DIRECTORY_NAME
+    Blog.CONTENT_DIRECTORY_NAME
   );
 
+  constructor(public readonly title: string, public readonly content: string) {
+    this.title = title;
+    this.content = content;
+  }
+
   public static getContentDirectoryName(): string {
-    return Blogs.CONTENT_DIRECTORY_NAME;
+    return Blog.CONTENT_DIRECTORY_NAME;
   }
 
   public static async getMarkdown(fileContent: string) {
-    return (await remark().use(remarkHtml).process(fileContent)).toString();
+    return {
+      markdownContent: (
+        await remark().use(remarkHtml).process(fileContent)
+      ).toString(),
+    };
   }
 
   public static async getBlog(title: string) {
@@ -28,12 +37,11 @@ export class Blogs {
       title,
       title + this.CONTENT_FORMAT
     );
-    const fileContent = readFileSync(absoluteFilePath).toString();
 
-    return { markdownContent: await this.getMarkdown(fileContent) };
+    return readFileSync(absoluteFilePath).toString();
   }
 
-  public static async getBlogs() {
+  public static async getBlogTitles() {
     return readdirSync(
       join(
         process.cwd(),
@@ -41,5 +49,18 @@ export class Blogs {
         this.CONTENT_DIRECTORY_NAME
       )
     );
+  }
+
+  public static async getBlogsObject() {
+    const blogTitles = await this.getBlogTitles();
+
+    const blogsObjects = new Array<Blog>();
+    for (const blogTitle of blogTitles) {
+      const blogContent = await this.getBlog(blogTitle);
+      const blog = new Blog(blogTitle, blogContent);
+      blogsObjects.push(blog);
+    }
+
+    return blogsObjects;
   }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,66 @@
 import Link from "next/link";
-
-import { Blogs } from "../lib/blogs";
+import { Blog } from "../lib/blog";
 
 type Props = {
-  blogTitles: Array<string>;
+  blogs: Array<Blog>;
   contentDirectoryName: string;
 };
 
-function Home({ blogTitles, contentDirectoryName }: Props) {
+function Home({ blogs, contentDirectoryName }: Props) {
   return (
-    <div className="blog__list">
-      <h1>Blog post list</h1>
+    <div className="blog-list">
       <ul>
-        {blogTitles.map((blogTitle: string) => (
-          <li key={blogTitle}>
+        {blogs.map((blog: Blog) => (
+          <li key={blog.title}>
             <Link
-              href={`/${contentDirectoryName}/${encodeURIComponent(blogTitle)}`}
+              href={`/${contentDirectoryName}/${encodeURIComponent(
+                blog.title
+              )}`}
             >
-              <a>{blogTitle}</a>
+              <a className="blog-list__title">{blog.title}</a>
             </Link>
+            <p className="blog-list__content">
+              {blog.content.length > 500
+                ? blog.content.substring(0, 500) + "..."
+                : blog.content}
+            </p>
           </li>
         ))}
       </ul>
+      <style jsx>
+        {`
+          .blog-list {
+            padding: 10vh 10vw;
+          }
+
+          .blog-list ul {
+            display: grid;
+            gap: 5vh;
+          }
+
+          .blog-list__title {
+            display: block;
+            margin-bottom: 0.5em;
+            font-weight: var(--font-weight-heavy);
+            font-size: 1.3em;
+          }
+
+          .blog-list__content {
+            font-size: 1em;
+            font-weight: var(--font-weight-normal);
+          }
+        `}
+      </style>
     </div>
   );
 }
 
 export async function getStaticProps() {
+  const blogs = JSON.stringify(await Blog.getBlogsObject());
   return {
     props: {
-      blogTitles: await Blogs.getBlogs(),
-      contentDirectoryName: Blogs.getContentDirectoryName(),
+      blogs: JSON.parse(blogs),
+      contentDirectoryName: Blog.getContentDirectoryName(),
     },
   };
 }
