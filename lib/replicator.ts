83c521d60c40dffecfd1cdb5721b598ad7ceba37
김재광 <45834750+JP3Pe@users.noreply.github.com>
2022-10-05 23:01:25 +0900

    .md 파일내 이미지 경로를 웹 URL에 맞추어 변경하는 기능 추가 (#53)
    * add: blogs 디렉토리를 publish/blogs으로 복사하는 기능 추가
    
    closes #52
    
    * fix: jsx 태그 에러로 인식하는 문제 수정
    
    closes #52
    
    * fix: 누락된 패키지 관련 정보 추가
    
    closes #52
    
    * add: 복사된 콘텐츠 파일의 이미지 URL을 웹용으로 수정하는 기능 추가
    
    closes #52

---
+++ lib/replicator.ts
@@ -0,0 +1,58 @@
+import { join } from "path";
+import { existsSync, readFileSync, writeFileSync } from "fs";
+import { copy } from "fs-extra";
+
+import { Blog } from "./blog";
+
+export class Replicator {
+  public static async isBlogContentsReplicated() {
+    return existsSync(Blog.CONTENT_DIRECTORY_ABSOLUTE_PATH);
+  }
+
+  public static async replicateBlogContents() {
+    const sourceDirectoryPath = join(
+      Blog.ROOT_DIRECTORY_PATH,
+      Blog.CONTENT_DIRECTORY_NAME
+    );
+    const destinationDirectoryPath = join(Blog.CONTENT_DIRECTORY_ABSOLUTE_PATH);
+    try {
+      // Copy original content directory into public directory
+      await copy(sourceDirectoryPath, destinationDirectoryPath);
+    } catch (err) {
+      console.error(err);
+    }
+
+    console.log(
+      `Successful copy directory from ${sourceDirectoryPath} to ${destinationDirectoryPath}`
+    );
+  }
+
+  public static async markdownImageUrlChanger(
+    blogTitle: string,
+    blogContent: string
+  ) {
+    return blogContent.replace(/\]\(\S*.\//g, `](${blogTitle}/`);
+  }
+
+  public static async modifyBlogContent(blogTitle: string) {
+    const fileAbsolutePath = join(
+      Blog.CONTENT_DIRECTORY_ABSOLUTE_PATH,
+      blogTitle,
+      blogTitle + Blog.CONTENT_FORMAT
+    );
+    const fileContent = readFileSync(fileAbsolutePath).toString();
+    const convertedFileContent = await this.markdownImageUrlChanger(
+      blogTitle,
+      fileContent
+    );
+    writeFileSync(fileAbsolutePath, convertedFileContent);
+  }
+
+  public static async modifyBlogContents() {
+    const blogTitles = await Blog.getBlogTitles();
+
+    for (const blogTitle of blogTitles) {
+      await this.modifyBlogContent(blogTitle);
+    }
+  }
+}
